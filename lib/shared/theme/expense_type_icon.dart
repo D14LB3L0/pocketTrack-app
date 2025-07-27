41cd466f208c50335ca0413fb43aada2f30d9ed1
D14LB3L0 <diegobdev2024@gmail.com>
2025-07-27 18:37:56 +0100

    icon

--- lib/shared/theme/expense_type_icon.dart
+++ lib/shared/theme/expense_type_icon.dart
@@ -5,11 +5,11 @@ import 'app_theme.dart';
 Icon expenseTypeIcon(String expenseType) {
   switch (expenseType.toLowerCase()) {
     case 'food':
-      return Icon(size: 50, Icons.fastfood, color: AppTheme.primaryColor);
+      return Icon(size: 40, Icons.fastfood, color: AppTheme.primaryColor);
     case 'transport':
-      return Icon(size: 50, Icons.directions_car, color: AppTheme.primaryColor);
+      return Icon(size: 40, Icons.directions_car, color: AppTheme.primaryColor);
     case 'entertainment':
-      return Icon(size: 50, Icons.movie, color: AppTheme.primaryColor);
+      return Icon(size: 40, Icons.movie, color: AppTheme.primaryColor);
     case 'health':
       return Icon(
         size: 50,
@@ -17,8 +17,8 @@ Icon expenseTypeIcon(String expenseType) {
         color: AppTheme.primaryColor,
       );
     case 'education':
-      return Icon(size: 50, Icons.school, color: AppTheme.primaryColor);
+      return Icon(size: 40, Icons.school, color: AppTheme.primaryColor);
     default:
-      return Icon(size: 50, Icons.help_outline, color: AppTheme.primaryColor);
+      return Icon(size: 40, Icons.help_outline, color: AppTheme.primaryColor);
   }
 }
